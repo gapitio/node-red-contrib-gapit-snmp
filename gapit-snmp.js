e28496e6fc20adfc33d5cbe32d272cd81b3e75e3
Sverre Johan Tøvik <sjt@gapit.io>
2021-10-14 14:49:54 +0200

    Add Scaling popup to node edit dialog, implement and use Scaling class.
    The "Scaling" class is initialized with the chosen scaling ("general" or
    "schleifenbauer", and use_scaling is set to method _scaling_general or
    _scaling_schleifenbauer. To add another scaling type, only a method
    named for the new scaling needs to be added, and it will be available
    for use (i.e. after being added to the edit node dialog). If a scaling
    method needs any initialization, an additional "_init_scaling" method
    may be implemented (like "_init_scaling_schleifenbauer").
    
    Scaling is applied for any value which is not of byte_type STR.

--- gapit-snmp.js
+++ gapit-snmp.js
@@ -53,6 +53,124 @@ module.exports = function (RED) {
     }
 
 
+    class Scaling {
+        constructor(name) {
+            // set use_scaling to requested scaling
+            var scaling_func = "_scaling_" + name;
+            if (this[scaling_func] === undefined) {
+                console.warn("Could not find scaling function '" + scaling_func + "', falling back to 'general'");
+                scaling_func = "_scaling_general";
+            }
+            else {
+                console.debug("Found scaling function '" + scaling_func + "'");
+            }
+            this.use_scaling = this[scaling_func];
+            var scaling_init_func = "_init_scaling_"  + name;
+            if (this[scaling_init_func] !== undefined) {
+                console.debug("Calling init for scaling '" + name + "'")
+                this[scaling_init_func]();
+            }
+        }
+
+        _init_scaling_schleifenbauer() {
+            this.registers = Object();
+            // each register field_name set needs its own set of registers
+            // these are lazy initialized when the first register of a 
+            // field_name is used
+        }
+        
+        _scaling_general(value, scaling_factor, unit, field_name) {
+            if (typeof value === "number" && typeof scaling_factor === "number" && scaling_factor != 1) {
+                // cast to string with 8 decimals, and convert back to number
+                // this is to avoid numbers like 49.900000000000006 (from 499 * 0.1)
+                var result = Number((value * scaling_factor).toFixed(8));
+                console.debug(`Applied scaling to value ${value} with factor ${scaling_factor}, for result ${result}`);
+                return result
+            }
+            else if (scaling_factor == 1) {
+                console.warn("scaling_factor == 1, returning unchanged value");
+                return value;
+            }
+            else {
+                console.warn("Value or scaling_factor is not a number, returning unchanged value")
+                return value;
+            }
+        }
+
+        _scaling_schleifenbauer(value, scaling_factor, unit, field_name) {
+            console.debug(`Decoding Schleifenbauer with value ${value} and scaling factor ${scaling_factor}`);
+
+            if (typeof value === "number" && typeof scaling_factor === "number" && scaling_factor != 1) {
+                // cast to string with 8 fixed decimals, and convert back to number
+                // this to avoid numbers like 49.900000000000006
+                var result = Number((value * scaling_factor).toFixed(8));
+                console.debug(`Applied scaling to value ${value} with factor ${scaling_factor}, for result ${result}`);
+            }
+            else if (scaling_factor == 1) {
+                console.warn("scaling_factor == 1, returning unchanged value");
+                return value;
+            }
+            else {
+                console.warn("Value or scaling_factor is not a number, returning unchanged value")
+                return value;
+            }
+
+            if (unit.startsWith("register")) {
+                // this is a register1/2/3 field
+                // only the last word of the field names should be different
+                // (e.g. "Active Total 1", "Active Total 2")
+
+                // find common field name ("Active Total" in above example)
+                var common_field_name = field_name.split(" ").slice(0, -1).join(" ")
+                //console.log("common_field_name: " + common_field_name);
+                // set up registers for common field name if missing
+                if (! (common_field_name in this.registers)) {
+                    console.log(`initializing registers for ${common_field_name}`);
+                    this.registers[common_field_name] = {
+                        "register1": -1, 
+                        "register2": -1, 
+                        "register3": -1
+                    }
+                }
+
+                if (unit == "register1") {
+                    this.registers[common_field_name]["register1"] = result;
+                }
+                else if (unit == "register2") {
+                    this.registers[common_field_name]["register2"] = result;
+                }
+                else if (unit == "register3") {
+                    this.registers[common_field_name]["register3"] = result;
+                }
+                else if (unit == "register4") {
+                    // if registers 1 through 3 are set (not -1), return sum
+                    if (this.registers[common_field_name]["register1"] != -1 &&
+                            this.registers[common_field_name]["register2"] != -1 && 
+                            this.registers[common_field_name]["register3"] != -1) {
+                        console.debug(`All registers set for '${common_field_name}', calculating total`);
+                        result = this.registers[common_field_name]["register1"] + 
+                            this.registers[common_field_name]["register2"] + 
+                            this.registers[common_field_name]["register3"]
+                        // reset registers
+                        this.registers[common_field_name]["register1"] = -1;
+                        this.registers[common_field_name]["register2"] = -1;
+                        this.registers[common_field_name]["register3"] = -1;
+                    }
+                    else {
+                        console.debug(`One or more registers was not set for '${common_field_name}', cannot calculate total`);
+                        // reset registers
+                        this.registers[common_field_name]["register1"] = -1;
+                        this.registers[common_field_name]["register2"] = -1;
+                        this.registers[common_field_name]["register3"] = -1;
+                    }
+                }
+            }
+            return result;
+        }
+    
+    }
+
+
     function GapitSnmpNode(config) {
         RED.nodes.createNode(this, config);
         this.community = config.community;
@@ -61,6 +179,7 @@ module.exports = function (RED) {
         if (config.gapit_code) {
             this.gapit_code = JSON.parse(config.gapit_code);
         }
+        this.scaling = config.scaling;
         this.skip_nonexistent_oids = config.skip_nonexistent_oids;
         this.remove_novalue_items_from_gapit_results = config.remove_novalue_items_from_gapit_results;
         this.timeout = Number(config.timeout || 5) * 1000;
@@ -84,6 +203,9 @@ module.exports = function (RED) {
         for (const [key, val] of Object.entries(this.db_tags)) {
             console.debug("   " + key + ": " + val);
         }*/
+
+        this.scaler = new Scaling(this.scaling);
+
         var node = this;
 
         // get context
@@ -192,7 +314,19 @@ module.exports = function (RED) {
                                 for (var member_idx = groups[group_idx]["group"].length - 1; member_idx >= 0 ; member_idx--) { 
                                     var oid = groups[group_idx]["group"][member_idx]["address"];
                                     if (oid in oid_value_map) {
-                                        groups[group_idx]["group"][member_idx]["value"] = oid_value_map[oid];
+                                        
+                                        if(groups[group_idx]["group"][member_idx]["byte_type"] != "STR") {
+                                            // not a string, apply scaling
+                                            groups[group_idx]["group"][member_idx]["value"] = 
+                                                node.scaler.use_scaling(oid_value_map[oid], 
+                                                                        groups[group_idx]["group"][member_idx]["scaling_factor"], 
+                                                                        groups[group_idx]["group"][member_idx]["unit"], 
+                                                                        groups[group_idx]["group"][member_idx]["description"]);
+                                        }
+                                        else {
+                                            // no scaling for string values
+                                            groups[group_idx]["group"][member_idx]["value"] = oid_value_map[oid];
+                                        }
                                     }
                                     else if (remove_novalue_items_from_gapit_results) {
                                         groups[group_idx]["group"].splice(member_idx, 1);
